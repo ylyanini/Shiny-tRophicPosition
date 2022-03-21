abe671fa87e1cf3232fad2ed5964655c32560f3d
Yan Lucaveche <yan.lucaveche@gmail.com>
2022-03-21 17:32:24 -0300

    21/03 - inicio practica

---
+++ test/app.R
@@ -0,0 +1,133 @@
+  #Funcionalidad 1: Ingreso de información
+  #Funcionalidad 2: chequeo y graficado de información
+  #Funcionalidad 3: Establecer factor de discriminacion trofica (deltaN y/o deltaC)
+  #Funcionalidad 4: Establecer modelo Bayesiano
+  #Funcionalidad 5: Crear modelo JAGS/BUGS
+  #Funcionalidad 6: Generar muestras a posteriori
+  #Funcionalidad 7: Combinar, resumir gy graficar muestras a posteriori
+
+  #objetivo de la implementacion
+  #greta como formato sobre bugs
+  #metricas
+  #tanto comparacion de rendimiento como nuevas funcionalidades como publicacion
+
+
+  #isotop data a distintos formatos
+  #exportacion de luis para la carcasa
+  #dejar claro que funcionalidades hare yo, y que se elevara con shiny
+  #ejemplificaciones visuales en el informe
+
+  library(tRophicPosition)
+  library(shiny)
+  library(shinythemes)
+  library(data.table)
+  library(ggplot2)
+  library(bslib)
+
+  ui <- fluidPage(
+    theme = bs_theme(bootswatch = "pulse"),
+
+    navbarPage(inverse = TRUE , position = c("static-top", "fixed-top", "fixed-bottom"),
+      "App Title",
+               tabPanel("Plot"),
+               navbarMenu("More",
+                          tabPanel("Summary"),
+                          "----",
+                          "Section header",
+                          tabPanel("Table")
+               )
+    ),
+
+    titlePanel("Ingrese los datos para su analizis"),
+
+    sidebarLayout(
+
+      sidebarPanel(
+        'Example text'
+      ),
+
+      mainPanel(
+
+        tabsetPanel(
+          tabPanel('Resumen de datos', fluid = TRUE,
+          ),
+          tabPanel('Tabla de datos', fluid = TRUE,
+
+          ),
+          tabPanel('Graficos de datos', fluid = TRUE,
+
+          )
+        )
+      )
+    )
+  )
+
+
+  server <- function(input,output) {
+
+  }
+
+  shinyApp(ui = ui, server = server)
+
+
+  #   ## input$file is a data frame and contains the details around the name, size and temp location of the files uploaded
+  #   # this reactive output display the content of the input$file dataframe
+  #   output$filedf <- renderTable({
+  #     if(is.null(input$file)){return ()}
+  #     input$file # the file input data frame object that contains the file attributes
+  #   })
+  #
+  #   # Extract the file path for file
+  #   output$filedf2 <- renderTable({
+  #     if(is.null(input$file)){return ()}
+  #     input$file$datapath # the file input data frame object that contains the file attributes
+  #   })
+  #
+  #   ## Below code to display the structure of the input file object
+  #   output$fileob <- renderPrint({
+  #     if(is.null(input$file)){return ()}
+  #     str(input$file)
+  #   })
+  #
+  #   ## Side bar select input widget coming through renderUI()
+  #   # Following code displays the select input widget with the list of file loaded by the user
+  #   output$selectfile <- renderUI({
+  #     if(is.null(input$file)) {return()}
+  #     list(hr(),
+  #          helpText("Seleccione el documento con el cual se trabajara"),
+  #          selectInput("Select", "Archivos disponibles: ",
+  #                      choices=input$file$name)
+  #     )
+  #
+  #   })
+  #
+  #   ## Summary Stats code ##
+  #   # this reactive output contains the summary of the dataset and display the summary in table format
+  #   output$summ <- renderPrint({
+  #     if(is.null(input$file)){return()}
+  #     summary(read.table(file=input$file$datapath[input$file$name==input$Select],
+  #                        sep=input$sep,
+  #                        header = input$header,
+  #                        stringsAsFactors = input$stringAsFactors))})
+  #
+  #   ## Dataset code ##
+  #   # This reactive output contains the dataset and display the dataset in table format
+  #   output$table <- renderTable({
+  #     if(is.null(input$file)){return()}
+  #     read.table(file=input$file$datapath[input$file$name==input$Select], sep=input$sep, header = input$header, stringsAsFactors = input$stringAsFactors)
+  #
+  #   })
+  #
+  #   ## MainPanel tabset renderUI code ##
+  #   # the following renderUI is used to dynamically generate the tabsets when the file is loaded.
+  #   # Until the file is loaded, app will not show the tabset.
+  #   output$tb <- renderUI({
+  #     if(is.null(input$file)) {return()}
+  #     else
+  #       tabsetPanel(
+  #         tabPanel("Archivo de entrada ", tableOutput("filedf"), tableOutput("filedf2")),
+  #         tabPanel("Estructura del objeto de entrada", verbatimTextOutput("fileob")),
+  #         tabPanel("Tabla de contenido", tableOutput("table")),
+  #         tabPanel("Resumen de Informacion", verbatimTextOutput("summ")))
+  #   })
+
