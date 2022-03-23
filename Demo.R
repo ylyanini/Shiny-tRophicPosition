9714a90dca3d1810a82a24799e30edf5588d403e
Yan Lucaveche <yan.lucaveche@gmail.com>
2022-03-23 17:55:42 -0300

    23/03 - pruebas de presonalisacion e integracion de estilos personalisados

--- Demo.R
+++ Demo.R
@@ -26,124 +26,165 @@ library(ggplot2)
 
 
 
-ui <- fluidPage(theme = shinytheme("cosmo"),
-
-
-
-                navbarPage(title = "Demo Anteproyecto", fluid = TRUE,
-                           tabPanel("Plot",
-                                    tabsetPanel(
-
-                                      tabPanel("Set de Datos", fluid = TRUE,
-                                               sidebarLayout(
-                                                 sidebarPanel(
-
-                                                   fileInput("file",
-                                                             label = "Ingrese archivos por analizar",
-                                                             placeholder = "Ejemplo.csv",
-                                                             buttonLabel = "Buscar",
-                                                             multiple = TRUE,),
-                                                   helpText("Solo con extenciones .csv"),
-
-                                                   checkboxInput(inputId = 'header',
-                                                                 label = 'Header',
-                                                                 value = TRUE),
-                                                   checkboxInput(inputId = "stringAsFactors", "stringAsFactors", FALSE),
-
-                                                   radioButtons(inputId = 'sep',
-                                                                label = 'Separator',
-                                                                choices = c(Coma=',',PuntoyComa=';',Tabulacion='\t', Espacio=' '),
-                                                                selected = ','),
-
-                                                   uiOutput("selectfile")
-                                                 ),
-                                                 mainPanel(
-                                                   uiOutput("tb")
-
-                                                 )
-
-                                               )
-
-
-                                      ),
-                                      tabPanel("Tablas", fluid = TRUE),
-                                      tabPanel("Graficos", fluid = TRUE)
-
-                                    )
-                                    ),
-                           navbarMenu("More",
-                                      tabPanel("Summary"),
-                                      "----",
-                                      "Section header",
-                                      tabPanel("Table")
-                           )
-                ),
-
-
-
-
-
+ui <- fluidPage(
+  theme = shinytheme("cosmo"),
+  
+  
+  
+  navbarPage(
+    title = "Demo Anteproyecto",
+    fluid = TRUE,
+    tabPanel("Plot",
+             tabsetPanel(
+               tabPanel(
+                 "Set de Datos",
+                 fluid = TRUE,
+                 sidebarLayout(
+                   sidebarPanel(
+                     fileInput(
+                       "file",
+                       label = "Ingrese archivos por analizar",
+                       placeholder = "Ejemplo.csv",
+                       buttonLabel = "Buscar",
+                       multiple = TRUE,
+                     ),
+                     helpText("Solo con extenciones .csv"),
+                     
+                     checkboxInput(
+                       inputId = 'header',
+                       label = 'Header',
+                       value = TRUE
+                     ),
+                     checkboxInput(inputId = "stringAsFactors", "stringAsFactors", FALSE),
+                     
+                     radioButtons(
+                       inputId = 'sep',
+                       label = 'Separator',
+                       choices = c(
+                         Coma = ',',
+                         PuntoyComa = ';',
+                         Tabulacion = '\t',
+                         Espacio = ' '
+                       ),
+                       selected = ','
+                     ),
+                     
+                     uiOutput("selectfile")
+                   ),
+                   mainPanel(uiOutput("tb"))
+                   
+                 )
+                 
+                 
+               ),
+               tabPanel("Tablas", fluid = TRUE),
+               tabPanel("Graficos", fluid = TRUE)
+               
+             )),
+    navbarMenu(
+      "More",
+      tabPanel("Summary"),
+      "----",
+      "Section header",
+      tabPanel("Table")
+    )
+  ),
+  
+  
+  
+  
+  
 )
 
 
 
 
-server <- function(input,output) {
-
-
-
+server <- function(input, output) {
   output$filedf <- renderTable({
-    if(is.null(input$file)){return ()}
+    if (is.null(input$file)) {
+      return ()
+    }
     input$file
   })
-
-
+  
+  
   output$filedf2 <- renderTable({
-    if(is.null(input$file)){return ()}
+    if (is.null(input$file)) {
+      return ()
+    }
     input$file$datapath
   })
-
-
+  
+  
   output$fileob <- renderPrint({
-    if(is.null(input$file)){return ()}
+    if (is.null(input$file)) {
+      return ()
+    }
     str(input$file)
   })
-
-
+  
+  
   output$selectfile <- renderUI({
-    if(is.null(input$file)) {return()}
-    list(hr(),
-         helpText("Seleccione el documento con el cual se trabajara"),
-         selectInput("Select", "Archivos disponibles: ",
-                     choices=input$file$name)
+    if (is.null(input$file)) {
+      return()
+    }
+    list(
+      hr(),
+      helpText("Seleccione el documento con el cual se trabajara"),
+      selectInput("Select", "Archivos disponibles: ",
+                  choices = input$file$name)
     )
-
+    
   })
-
-
+  
+  
   output$summ <- renderPrint({
-    if(is.null(input$file)){return()}
-    summary(read.table(file=input$file$datapath[input$file$name==input$Select],
-                       sep=input$sep,
-                       header = input$header,
-                       stringsAsFactors = input$stringAsFactors))})
-
-
-  output$table <- renderTable({
-    if(is.null(input$file)){return()}
-    read.table(file=input$file$datapath[input$file$name==input$Select], sep=input$sep, header = input$header, stringsAsFactors = input$stringAsFactors)
-
+    if (is.null(input$file)) {
+      return()
+    }
+    summary(
+      read.table(
+        file = input$file$datapath[input$file$name == input$Select],
+        sep = input$sep,
+        header = input$header,
+        stringsAsFactors = input$stringAsFactors
+      )
+    )
   })
-
-
+  
+  
+  output$table <- renderTable({
+    if (is.null(input$file)) {
+      return()
+    }
+    read.table(
+      file = input$file$datapath[input$file$name == input$Select],
+      sep = input$sep,
+      header = input$header,
+      stringsAsFactors = input$stringAsFactors
+    )
+    
+  })
+  
+  
   output$tb <- renderUI({
-    if(is.null(input$file)) {return()}
+    if (is.null(input$file)) {
+      return()
+    }
     else
       tabsetPanel(
-        tabPanel("Archivo de entrada ", tableOutput("filedf"), tableOutput("filedf2")),
-        tabPanel("Estructura del objeto de entrada", verbatimTextOutput("fileob")),
+        tabPanel(
+          "Archivo de entrada ",
+          tableOutput("filedf"),
+          tableOutput("filedf2")
+        ),
+        tabPanel(
+          "Estructura del objeto de entrada",
+          verbatimTextOutput("fileob")
+        ),
         tabPanel("Tabla de contenido", tableOutput("table")),
-        tabPanel("Resumen de Informacion", verbatimTextOutput("summ")))
+        tabPanel("Resumen de Informacion", verbatimTextOutput("summ"))
+      )
   })
 }
 
--- test/app.R
+++ test/app.R
@@ -1,22 +1,22 @@
-  #rTrophicPosition 
-  #Funcionalidad 1: Ingreso de información
-  #Funcionalidad 2: chequeo y graficado de información
-  #Funcionalidad 3: Establecer factor de discriminacion trofica (deltaN y/o deltaC)
-  #Funcionalidad 4: Establecer modelo Bayesiano
-  #Funcionalidad 5: Crear modelo JAGS/BUGS
-  #Funcionalidad 6: Generar muestras a posteriori
-  #Funcionalidad 7: Combinar, resumir gy graficar muestras a posteriori
+#rTrophicPosition
+#Funcionalidad 1: Ingreso de información
+#Funcionalidad 2: chequeo y graficado de información
+#Funcionalidad 3: Establecer factor de discriminacion trofica (deltaN y/o deltaC)
+#Funcionalidad 4: Establecer modelo Bayesiano
+#Funcionalidad 5: Crear modelo JAGS/BUGS
+#Funcionalidad 6: Generar muestras a posteriori
+#Funcionalidad 7: Combinar, resumir gy graficar muestras a posteriori
 
-  #objetivo de la implementacion
-  #greta como formato sobre bugs
-  #metricas
-  #tanto comparacion de rendimiento como nuevas funcionalidades como publicacion
+#objetivo de la implementacion
+#greta como formato sobre bugs
+#metricas
+#tanto comparacion de rendimiento como nuevas funcionalidades como publicacion
 
 
-  #isotop data a distintos formatos
-  #exportacion de luis para la carcasa
-  #dejar claro que funcionalidades hare yo, y que se elevara con shiny
-  #ejemplificaciones visuales en el informe
+#isotop data a distintos formatos
+#exportacion de luis para la carcasa
+#dejar claro que funcionalidades hare yo, y que se elevara con shiny
+#ejemplificaciones visuales en el informe
 
 
 # 1)	Recolección de los datos otorgados por el usuario o la misma aplicación.
@@ -30,128 +30,158 @@
 
 
 
-  library(tRophicPosition)
-  library(shiny)
-  library(shinythemes)
-  library(data.table)
-  library(ggplot2)
-  library(bslib)
+library(tRophicPosition)
+library(shiny)
+library(shinythemes)
+library(data.table)
+library(ggplot2)
+library(bslib)
 
-  ui <- fluidPage(
-    
-    shiny::tags$head(
-      includeCSS("www/css/style.css")  
+ui <- fluidPage(
+  shiny::tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+  ),
+
+  
+  # theme = bs_theme(bootswatch = "minty"),
+  
+  navbarPage(
+    inverse = TRUE ,
+    position = c("static-top", "fixed-top", "fixed-bottom"),
+    "tRopicPosition",
+    # tabPanel("Plot"),
+    # navbarMenu("More",
+    #            tabPanel("Summary"),
+    #            "----",
+    #            "Section header",
+    #            tabPanel("Table")
+    # )
+  ),
+  
+  # titlePanel("Ingrese los datos para su analizis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      h4('Ingrese los datos para su analizis'),
+      
+      # fileInput("file",
+      #           label = "Ingrese archivos por analizar",
+      #           placeholder = "Ejemplo.csv",
+      #           buttonLabel = "Buscar",
+      #           multiple = TRUE,),
+      
+      fileInput("file1", "Choose CSV File", accept = c(".csv")),
+      checkboxInput("header", "Header", TRUE)
     ),
     
-    theme = bs_theme(bootswatch = "minty"),
+    mainPanel(tabsetPanel(
+      tabPanel('Resumen de datos', fluid = TRUE,
+               fluidRow(
+                 column(12,
+                        "Fluid 12",
+                        fluidRow(
+                          column(6,
+                                 "Fluid 6",
+                                 fluidRow(
+                                   column(6, 
+                                          "Fluid 6"),
+                                   column(6,
+                                          "Fluid 6")
+                                 )
+                          ),
+                          column(width = 6,
+                                 "Fluid 6")
+                        )
+                 )
+               )
+               ),
+      tabPanel('Tabla de datos', fluid = TRUE,
+               tableOutput('table')),
+      tabPanel('Graficos de datos', fluid = TRUE,)
+    ))
+  ),
+  tags$footer(HTML("
+                    <!-- Footer -->
+                           <footer class='page-footer font-large indigo'>
+                           <!-- Copyright -->
+                           <div class='footer-copyright text-center py-3'>© 2022 Copyright tRopicPosition
+                           </div>
+                           <!-- Copyright -->
 
-    navbarPage(inverse = TRUE , position = c("static-top", "fixed-top", "fixed-bottom"),
-      "tRopicPosition",
-               # tabPanel("Plot"),
-               # navbarMenu("More",
-               #            tabPanel("Summary"),
-               #            "----",
-               #            "Section header",
-               #            tabPanel("Table")
-               # )
-    ),
-
-    # titlePanel("Ingrese los datos para su analizis"),
-
-    sidebarLayout(
-
-      sidebarPanel(
-        h4('Ingrese los datos para su analizis'),
-        
-        fileInput("file",
-                  label = "Ingrese archivos por analizar",
-                  placeholder = "Ejemplo.csv",
-                  buttonLabel = "Buscar",
-                  multiple = TRUE,),
-      ),
-
-      mainPanel(
-
-        tabsetPanel(
-          tabPanel('Resumen de datos', fluid = TRUE,
-          ),
-          tabPanel('Tabla de datos', fluid = TRUE,
-
-          ),
-          tabPanel('Graficos de datos', fluid = TRUE,
-
-          )
-        )
-      )
-    )
-  )
+                           </footer>
+                           <!-- Footer -->")),
+  
+)
 
 
-  server <- function(input,output) {
 
-  }
+server <- function(input, output) {
+  output$table <- renderTable({
+    input$file1
+  })
+  
+}
 
-  shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server)
 
 
-  #   ## input$file is a data frame and contains the details around the name, size and temp location of the files uploaded
-  #   # this reactive output display the content of the input$file dataframe
-  #   output$filedf <- renderTable({
-  #     if(is.null(input$file)){return ()}
-  #     input$file # the file input data frame object that contains the file attributes
-  #   })
-  #
-  #   # Extract the file path for file
-  #   output$filedf2 <- renderTable({
-  #     if(is.null(input$file)){return ()}
-  #     input$file$datapath # the file input data frame object that contains the file attributes
-  #   })
-  #
-  #   ## Below code to display the structure of the input file object
-  #   output$fileob <- renderPrint({
-  #     if(is.null(input$file)){return ()}
-  #     str(input$file)
-  #   })
-  #
-  #   ## Side bar select input widget coming through renderUI()
-  #   # Following code displays the select input widget with the list of file loaded by the user
-  #   output$selectfile <- renderUI({
-  #     if(is.null(input$file)) {return()}
-  #     list(hr(),
-  #          helpText("Seleccione el documento con el cual se trabajara"),
-  #          selectInput("Select", "Archivos disponibles: ",
-  #                      choices=input$file$name)
-  #     )
-  #
-  #   })
-  #
-  #   ## Summary Stats code ##
-  #   # this reactive output contains the summary of the dataset and display the summary in table format
-  #   output$summ <- renderPrint({
-  #     if(is.null(input$file)){return()}
-  #     summary(read.table(file=input$file$datapath[input$file$name==input$Select],
-  #                        sep=input$sep,
-  #                        header = input$header,
-  #                        stringsAsFactors = input$stringAsFactors))})
-  #
-  #   ## Dataset code ##
-  #   # This reactive output contains the dataset and display the dataset in table format
-  #   output$table <- renderTable({
-  #     if(is.null(input$file)){return()}
-  #     read.table(file=input$file$datapath[input$file$name==input$Select], sep=input$sep, header = input$header, stringsAsFactors = input$stringAsFactors)
-  #
-  #   })
-  #
-  #   ## MainPanel tabset renderUI code ##
-  #   # the following renderUI is used to dynamically generate the tabsets when the file is loaded.
-  #   # Until the file is loaded, app will not show the tabset.
-  #   output$tb <- renderUI({
-  #     if(is.null(input$file)) {return()}
-  #     else
-  #       tabsetPanel(
-  #         tabPanel("Archivo de entrada ", tableOutput("filedf"), tableOutput("filedf2")),
-  #         tabPanel("Estructura del objeto de entrada", verbatimTextOutput("fileob")),
-  #         tabPanel("Tabla de contenido", tableOutput("table")),
-  #         tabPanel("Resumen de Informacion", verbatimTextOutput("summ")))
-  #   })
-
+#   ## input$file is a data frame and contains the details around the name, size and temp location of the files uploaded
+#   # this reactive output display the content of the input$file dataframe
+#   output$filedf <- renderTable({
+#     if(is.null(input$file)){return ()}
+#     input$file # the file input data frame object that contains the file attributes
+#   })
+#
+#   # Extract the file path for file
+#   output$filedf2 <- renderTable({
+#     if(is.null(input$file)){return ()}
+#     input$file$datapath # the file input data frame object that contains the file attributes
+#   })
+#
+#   ## Below code to display the structure of the input file object
+#   output$fileob <- renderPrint({
+#     if(is.null(input$file)){return ()}
+#     str(input$file)
+#   })
+#
+#   ## Side bar select input widget coming through renderUI()
+#   # Following code displays the select input widget with the list of file loaded by the user
+#   output$selectfile <- renderUI({
+#     if(is.null(input$file)) {return()}
+#     list(hr(),
+#          helpText("Seleccione el documento con el cual se trabajara"),
+#          selectInput("Select", "Archivos disponibles: ",
+#                      choices=input$file$name)
+#     )
+#
+#   })
+#
+#   ## Summary Stats code ##
+#   # this reactive output contains the summary of the dataset and display the summary in table format
+#   output$summ <- renderPrint({
+#     if(is.null(input$file)){return()}
+#     summary(read.table(file=input$file$datapath[input$file$name==input$Select],
+#                        sep=input$sep,
+#                        header = input$header,
+#                        stringsAsFactors = input$stringAsFactors))})
+#
+#   ## Dataset code ##
+#   # This reactive output contains the dataset and display the dataset in table format
+#   output$table <- renderTable({
+#     if(is.null(input$file)){return()}
+#     read.table(file=input$file$datapath[input$file$name==input$Select], sep=input$sep, header = input$header, stringsAsFactors = input$stringAsFactors)
+#
+#   })
+#
+#   ## MainPanel tabset renderUI code ##
+#   # the following renderUI is used to dynamically generate the tabsets when the file is loaded.
+#   # Until the file is loaded, app will not show the tabset.
+#   output$tb <- renderUI({
+#     if(is.null(input$file)) {return()}
+#     else
+#       tabsetPanel(
+#         tabPanel("Archivo de entrada ", tableOutput("filedf"), tableOutput("filedf2")),
+#         tabPanel("Estructura del objeto de entrada", verbatimTextOutput("fileob")),
+#         tabPanel("Tabla de contenido", tableOutput("table")),
+#         tabPanel("Resumen de Informacion", verbatimTextOutput("summ")))
+#   })
