8b83cfbafcc8b5b904de9f795648b57d0cbe1af4
Yan Lucaveche <yan.lucaveche@gmail.com>
2022-03-29 17:45:54 -0300

    incorporacion de reactividad - 29/03

--- test/logica/app.R
+++ test/logica/app.R
@@ -1,81 +1,140 @@
 
+
 library(shiny)
 library(DT)
 library(tRophicPosition)
 
 
-ui <- shinyUI(fluidPage(
-  
-  tags$h1("Logica"),
-  sidebarLayout(
-    sidebarPanel(
-      tags$h2("Upload file"),
-      fileInput('file', 'Choose file to upload',
-                accept = c(
-                  'text/csv',
-                  'text/comma-separated-values',
-                  '.csv'
-                )),
-      checkboxInput("header", "Header", TRUE),
-      radioButtons("sep",
-                   "Separator: ",
-                   choices = c(Comma = ",",
-                               Semicolon = ";",
-                               Tab = "\t"),
-                   selected=",",
-                   inline=TRUE),
-      
-      
-            radioButtons(
-              "quote",
-              "Quote",
-              choices = c(
-                None = "",
-                "Double Quote" = '"',
-                "Single Quote" = "'"
-              ),
-              selected = '"'
-            ),
-            # Input: Select number of rows to display ----
-            radioButtons(
-              "disp",
-              "Display",
-              choices = c(Head = "head",
-                          All = "all"),
-              selected = "head"
-            ),
-            tags$hr(),
-            tags$h2('tRophicPosition'),
-            
-      
-    ),
-    mainPanel(
-      fluidRow(
-        column(width = 12,
-               fluidRow(tags$h2("Table"),
-                        DT::dataTableOutput("sample_table"))),
-        column(width = 12,
-               fluidPage(tags$h2("Data Summary"),
-                         verbatimTextOutput('summary'))),
-        column(width = 12,
-               fluidPage(tags$h2("Plot"), 
-                         plotOutput('plot'))),
-        
-        column(width = 12,
-               fluidPage(tags$h2("Posterior Summary"), 
-                         verbatimTextOutput('post_summary'))),
-               
-              
-        
-      ),
-      
-    )
-  ),
-  
- 
-  
-)
-)
+ui <- shinyUI(fluidPage(tags$h1("Logica"),
+                        sidebarLayout(
+                          sidebarPanel(
+                            tags$h2("Upload file"),
+                            fileInput(
+                              'file',
+                              'Choose file to upload',
+                              accept = c('text/csv',
+                                         'text/comma-separated-values',
+                                         '.csv')
+                            ),
+                            checkboxInput("header", "Header", TRUE),
+                            radioButtons(
+                              "sep",
+                              "Separator: ",
+                              choices = c(
+                                Comma = ",",
+                                Semicolon = ";",
+                                Tab = "\t"
+                              ),
+                              selected = ",",
+                              inline = TRUE
+                            ),
+                            
+                            
+                            radioButtons(
+                              "quote",
+                              "Quote",
+                              choices = c(
+                                None = "",
+                                "Double Quote" = '"',
+                                "Single Quote" = "'"
+                              ),
+                              selected = '"'
+                            ),
+                            # Input: Select number of rows to display ----
+                            radioButtons(
+                              "disp",
+                              "Display",
+                              choices = c(Head = "head",
+                                          All = "all"),
+                              selected = "head"
+                            ),
+                            tags$hr(),
+                            tags$h2('tRophicPosition'),
+                            
+                            
+                            
+                            tags$h3('Model'),
+                            numericInput('n.chains',
+                                         h4('n.chains'),
+                                         value = 4),
+                            numericInput('n.adapt',
+                                         h4('n.adapt'),
+                                         value = 5000),
+
+                            
+                                                        
+                            tags$h3('posterior.samples'),
+                            numericInput('n.iter',
+                                         h4('n.iter'),
+                                         value = 15000),
+                            
+                            
+                            
+                            tags$h3('loadIsotopeData'),
+                            textInput('consumer',
+                                      h4('consumer'),
+                                      value = 'Bilagay'),
+                            
+                            textInput('consumersColumn',
+                                      h4('consumersColumn'),
+                                      value = 'FG'),
+                            
+                            textInput('b1',
+                                      h4('b1'),
+                                      value = 'Pelagic_BL'),
+                            
+                            textInput('b2',
+                                      h4('b2'),
+                                      value = 'Benthic_BL'),
+                            
+                            textInput('baselineColumn',
+                                      h4('baselineColumn'),
+                                      value = 'FG'),
+                            
+                            textInput('group',
+                                         h4('group'),
+                                         value = 'Coquimbo'),
+                            
+                            textInput('groupsColumn',
+                                      h4('groupsColumn'),
+                                      value = 'Location'),
+                            
+                            h3("Buttons"),
+                            actionButton("action", "Action"),
+                            br(),
+                            br(), 
+                            submitButton("Submit"),
+                            
+                            
+                            
+                            
+                          ),
+                          mainPanel(fluidRow(
+                            column(width = 12,
+                                   fluidRow(
+                                     tags$h2("Table"),
+                                     DT::dataTableOutput("sample_table")
+                                   )),
+                            column(width = 12,
+                                   fluidPage(
+                                     tags$h2("Data Summary"),
+                                     verbatimTextOutput('summary')
+                                   )),
+                            
+                            column(width = 12,
+                                   fluidPage(
+                                     tags$h2("Posterior Summary"),
+                                     verbatimTextOutput('post_summary')
+                                   )),
+                            
+                            column(width = 12,
+                                   fluidPage(tags$h2("Plot"),
+                                             plotOutput('plot'),
+                                             plotOutput('plot_post'),
+                                             plotOutput('plot_TP'))),
+                            
+                          ),)
+                        ),))
 
 
 server <- shinyServer(function(input, output) {
@@ -86,68 +145,118 @@ server <- shinyServer(function(input, output) {
     inFile <- input$file
     if (is.null(inFile))
       return(NULL)
-    df <- read.csv(inFile$datapath, 
-                   header = input$header,
-                   sep = input$sep,
-                   quote = input$quote)
+    df <- read.csv(
+      inFile$datapath,
+      header = input$header,
+      sep = input$sep,
+      quote = input$quote
+    )
     
-
     return(df)
+    
   })
   
-  output$sample_table<- DT::renderDataTable({
+  isotopeData <- reactive({
+    df <- df_upload()
+    consumer <-
+      tRophicPosition::loadIsotopeData(
+        df,
+        consumer = input$consumer,
+        consumersColumn = input$consumersColumn,
+        b1 = input$b1,
+        b2 = input$b2,
+        baselineColumn = input$baselineColumn,
+        group = input$group,
+        groupsColumn = input$groupsColumn
+      )
+    
+    return(consumer)
+    
+  })
+  
+  posterior <- reactive({
+    
+    consumer <- isotopeData()
+    
+    model.string <-
+      tRophicPosition::jagsBayesianModel(model = "oneBaseline",
+                                         TP = "dnorm(4, 0.1)")
+    
+    model <-
+      tRophicPosition::TPmodel(
+        data = consumer,
+        model.string = model.string,
+        n.adapt = input$n.adapt,
+        n.chains = input$n.chains,
+        quiet = TRUE
+      )
+    
+    posterior.samples <-
+      tRophicPosition::posteriorTP(
+        model = model,
+        n.iter = input$n.iter,
+        variable.names = c("TP", "muDeltaN"),
+        quiet = TRUE
+      )
+    
+    return(posterior.samples)
+    
+  })
+  
+  
+  
+  output$sample_table <- DT::renderDataTable({
     df <- df_upload()
     DT::datatable(df)
   })
   
-  output$summary <-renderPrint({
+  output$summary <- renderPrint({
     summary(df_upload())
   })
   
   output$plot <- renderPlot({
-    df <- df_upload()
-    consumer <-
-      tRophicPosition::loadIsotopeData(
-        df,
-        consumer = "Bilagay",
-        consumersColumn = "FG",
-        b1 = "Pelagic_BL",
-        b2 = "Benthic_BL",
-        baselineColumn = "FG",
-        group = "Coquimbo",
-        groupsColumn = "Location"
-      )
-    plot(consumer, b1 = "Pelagic baseline", b2 = "Benthic baseline")
+    consumer <- isotopeData()
+    
+    plot(consumer)
+    
   })
   
-  output$post_summary <-renderPrint({
+  output$post_summary <- renderPrint({
+
+    posterior_samples <- posterior()
     
-    df <- df_upload()
-    consumer <-
-      tRophicPosition::loadIsotopeData(
-        df,
-        consumer = "Bilagay",
-        consumersColumn = "FG",
-        b1 = "Pelagic_BL",
-        b2 = "Benthic_BL",
-        baselineColumn = "FG",
-        group = "Coquimbo",
-        groupsColumn = "Location"
-      )
     
-    model.string <- tRophicPosition::jagsBayesianModel(model = "oneBaseline", TP = "dnorm(4, 0.1)")
-
-    model <- tRophicPosition::TPmodel(data = consumer, model.string = model.string,
-                     n.adapt = 5000, n.chains = 4, quiet = TRUE)
-
-    posterior.samples <- tRophicPosition::posteriorTP(model = model, n.iter = 15000,
-                                     variable.names = c("TP", "muDeltaN"),quiet = TRUE)
-
-
-    summary(posterior.samples)
+    summary(posterior_samples)
   })
   
-}
-)
- 
+  
+  output$plot_post <- renderPlot({
+    
+    posterior_samples <- posterior()
+    
+    posterior.combined <- coda::mcmc(do.call(rbind, posterior_samples))
+    
+    getPosteriorMode(posterior.combined)
+    
+    plot(posterior_samples)
+    
+    
+  })
+  
+
+  output$plot_TP <- renderPlot({
+    consumer <- isotopeData()
+    
+    posterior_samples <- posterior()
+    
+    combined <- as.data.frame(coda::mcmc(do.call(rbind, posterior_samples)))
+    
+    plotTP(combined, xlab = "Monitored variables")
+    
+    
+  })
+  
+
+})
+
 shinyApp(ui = ui, server = server)
