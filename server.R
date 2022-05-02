197f21259c2ba99e7ef90ea211d0ec60f87a45b3
Yan Lucaveche <yan.lucaveche@gmail.com>
2022-05-02 09:09:15 -0400

    TDF y modificaciones

--- server.R
+++ server.R
@@ -1,22 +1,30 @@
 
 
-
 library(shiny)
 library(DT)
 library(tRophicPosition)
 library(data.table)
 library(ggplot2)
-library(bslib)
 library(summarytools)
+library(waiter)
 
 shinyServer(function(input, output) {
+  hostess <- Hostess$new("loader")
+  
+  for (i in 1:10) {
+    Sys.sleep(runif(1) / 2)
+    hostess$set(i * 10)
+  }
+  
+  waiter_hide()
+  
   df_upload <- reactive({
     inFile <- input$file
     if (is.null(inFile))
       return(NULL)
     df <- read.csv(
       inFile$datapath,
-      header = input$header,
+      header = as.logical(input$header),
       sep = input$sep,
       quote = input$quote
     )
@@ -86,7 +94,7 @@ shinyServer(function(input, output) {
   
   output$screenfoodweb <- renderPlot({
     df <- df_upload()
-
+    
     screenFoodWeb(
       df,
       grouping = c("Spp", "FG"),
@@ -102,7 +110,7 @@ shinyServer(function(input, output) {
     plot(consumer)
     
   })
-    
+  
   output$post_summary <- renderPrint({
     posterior_samples <- posterior()
     
@@ -138,91 +146,9 @@ shinyServer(function(input, output) {
   })
   
   
-  output$content_data <- renderUI({
-    if (is.null(input$file)) {
-      return(fluidRow(
-        column(width = 12),
-        column(width = 3),
-        column(width = 7,
-               fluidRow(
-                 id = 'espera',
-                 tags$h2("Ingrese datos para su analisis", icon("table")),
-                 
-               )),
-        column(width = 2),
-      ))
-    } else{
-      df <- df_upload()
-      fluidRow(
-        column(
-          width = 12,
-          fluidPage(
-            id = 'resultados',
-            tags$h2("Data Table"),
-            DT::dataTableOutput("sample_table")
-          )
-        ),
-        column(
-          width = 12,
-          fluidPage(
-            id = 'resultados',
-            tags$h2("Data Summary"),
-            
-            st_options(dfSummary.custom.2 =
-                         expression(
-                           paste(
-                             "Q1 & Q4 :",
-                             
-                             quantile(
-                               column_data,
-                               probs = .25,
-                               type = 2,
-                               names = FALSE,
-                               na.rm = TRUE
-                             ),
-                             digits = 1
-                             ,
-                             " & ",
-                             
-                             quantile(
-                               column_data,
-                               probs = 1,
-                               type = 2,
-                               names = FALSE,
-                               na.rm = TRUE
-                             ),
-                             digits = 1
-                             
-                           )
-                         )),
-            
-            print(
-              dfSummary(
-                df,
-                varnumbers   = TRUE,
-                valid.col    = FALSE,
-                style        = "multiline",
-                graph.magnif = 1.5
-              ),
-              method   = 'render',
-              headings = FALSE,
-              bootstrap.css = FALSE
-            )
-            
-            
-          )
-        ),
-        column(
-          width = 12,
-          fluidPage(id = 'resultados',
-                    tags$h2("Data Species"),
-                    plotOutput('screenfoodweb'), )
-        )
-        
-      )
-    }
-    
-  })
+  
+  
+  
   
   
   output$content_TP <- renderUI({
@@ -233,21 +159,19 @@ shinyServer(function(input, output) {
         column(width = 7,
                fluidRow(
                  id = 'espera',
-                 tags$h2("Ingrese datos para su analisis", icon("table")),
+                 tags$h2("Upload your data to make some analisis", icon("thumbs-up")),
                )),
         column(width = 2),
       ))
     } else{
-      fluidRow(
-        column(
-          width = 12,
-          fluidPage(
-            id = 'resultados',
-            tags$h2("Plot Isotope Object"),
-            plotOutput('plot_content')
-          )
+      fluidRow(column(
+        width = 12,
+        fluidPage(
+          id = 'resultados',
+          tags$h2("Plot Isotope Object"),
+          plotOutput('plot_content')
         )
-      )
+      ))
     }
   })
   
@@ -260,30 +184,11 @@ shinyServer(function(input, output) {
         column(width = 7,
                fluidRow(
                  id = 'espera',
-                 tags$h2("Ingrese datos para su analisis", icon("table")),
+                 tags$h2("Upload your data to make some analisis", icon("thumbs-up")),
                )),
         column(width = 2),
       ))
-    }else{
-      
-    } 
-    
-  })
-  
-  
-  output$content_posterior <- renderUI({
-    if (is.null(input$file)) {
-      return(fluidRow(
-        column(width = 12),
-        column(width = 3),
-        column(width = 7,
-               fluidRow(
-                 id = 'espera',
-                 tags$h2("Ingrese datos para su analisis", icon("table")),
-               )),
-        column(width = 2),
-      ))
-    }else{
+    } else{
       fluidRow(
         column(
           width = 12,
@@ -306,8 +211,180 @@ shinyServer(function(input, output) {
                     plotOutput('plot_TP'))
         ),
       )
-    } 
+    }
     
   })
+  
+  observeEvent(input$create_model, {
+    output$panel_post_sample <- renderUI({
+      column(
+        width = 12,
+        tags$h2('Make Post Samples', icon("fas fa-chart-bar"),
+                style = "margin-top: 25px;"),
+        tags$hr(),
+        numericInput('n.iter',
+                     h4('n.iter'),
+                     value = 15000),
+        
+        actionBttn(
+          inputId = "make_post",
+          tags$h4("MAKE POST SAMPLES"),
+          style = "fill",
+          color = "success"
+        ),
+      )
+    })
+    
+  })
+  
+  #output$content_data <- renderUI({})
+    
+  output$content_data <- renderUI({
+    if (is.null(input$file)) {
+      output$content_data <- renderUI({
+        fluidRow(
+          column(width = 12),
+          column(width = 3),
+          column(width = 7,
+                 fluidRow(
+                   id = 'espera',
+                   tags$h2("Upload your data to make some analisis", icon("smile")),
+                   
+                 )),
+          column(width = 2),
+        )
+      })
+    }else{
+      observeEvent(input$up_file, {
+      output$content_data <- renderUI({
+        print("hola")
+    })
+    })
+    }
+    
+  })
+  #     else{
+  #       df <- df_upload()
+  #       fluidRow(
+  #         column(
+  #           width = 12,
+  #           fluidPage(
+  #             id = 'resultados',
+  #             tags$h2("Data Table"),
+  #             DT::dataTableOutput("sample_table")
+  #           )
+  #         ),
+  #         column(
+  #           width = 12,
+  #           fluidPage(
+  #             id = 'resultados',
+  #             tags$h2("Data Summary"),
+  #
+  #             st_options(dfSummary.custom.2 =
+  #                          expression(
+  #                            paste(
+  #                              "Q1 & Q4 :",
+  #
+  #                              quantile(
+  #                                column_data,
+  #                                probs = .25,
+  #                                type = 2,
+  #                                names = FALSE,
+  #                                na.rm = TRUE
+  #                              ),
+  #                              digits = 1
+  #                              ,
+  #                              " & ",
+  #
+  #                              quantile(
+  #                                column_data,
+  #                                probs = 1,
+  #                                type = 2,
+  #                                names = FALSE,
+  #                                na.rm = TRUE
+  #                              ),
+  #                              digits = 1
+  #
+  #                            )
+  #                          )),
+  #
+  #             print(
+  #               dfSummary(
+  #                 df,
+  #                 varnumbers   = TRUE,
+  #                 valid.col    = FALSE,
+  #                 style        = "multiline",
+  #                 graph.magnif = 1.5
+  #               ),
+  #               method   = 'render',
+  #               headings = FALSE,
+  #               bootstrap.css = FALSE
+  #             )
+  #
+  #
+  #           )
+  #         ),
+  #         column(
+  #           width = 12,
+  #           fluidPage(
+  #             id = 'resultados',
+  #             tags$h2("Data Species"),
+  #             plotOutput('screenfoodweb'),
+  #
+  #           )
+  #         )
+  #
+  #       )
+  #     }
+  #
+  #
+  # })
+  #
+  
+  observeEvent(input$TDF_author, {
+    output$type_select <- renderUI({
+      if (input$TDF_author == 'McCutchan (2003)') {
+        selectInput(
+          'type',
+          tags$h4('type'),
+          choices = c(
+            'All',
+            'Whole',
+            'Muscle',
+            'Acidified',
+            'Unacidified',
+            'Rainbow Trout',
+            'Brook Trout'
+          ),
+        )
+        
+      } else{
+        return()
+      }
+    })
+  })
+  
+  observeEvent(input$select_a_file, {
+    output$select_file <- renderUI({
+      if (input$select_a_file == 'New') {
+        fileInput(
+          'file',
+          tags$h3('Select File'),
+          accept = c('text/csv',
+                     'text/comma-separated-values',
+                     '.csv'),
+          buttonLabel = tags$b("UPLOAD"),
+          placeholder = "Example.csv",
+          multiple = FALSE,
+          
+        )
+        
+      } else{
+        return()
+      }
+    })
+  })
+  
+  #
   
 })
--- test/ui.R
+++ test/ui.R
@@ -1,11 +1,3 @@
-#Funcionalidad 1: Ingreso de información
-#Funcionalidad 2: chequeo y graficado de información
-#Funcionalidad 3: Establecer factor de discriminacion trofica (deltaN y/o deltaC)
-#Funcionalidad 4: Establecer modelo Bayesiano
-#Funcionalidad 5: Crear modelo JAGS/BUGS
-#Funcionalidad 6: Generar muestras a posteriori
-#Funcionalidad 7: Combinar, resumir gy graficar muestras a posteriori
-
 
 library(shiny)
 library(DT)
@@ -13,173 +5,204 @@ library(tRophicPosition)
 library(ggplot2)
 library(shinyWidgets)
 library(summarytools)
-library(bslib)
+library(waiter)
 
 
-
-shinyUI(fluidPage(
-  tags$head(
-    tags$link(rel = "stylesheet",
-              type = "text/css",
-              href = "style.css"),
-  ),
-  tags$head(
-    HTML('<link rel="icon", href="img/logo.png",
-                                   type="image/png" />')
-  ),
-  
-  navbarPage(
-    title = div(
-      img(
-        src = 'img/logo.png',
-        style = "margin-top: -14px;
-                               padding-right:9px;
-                               padding-bottom:9px",
-        height = 100
-      )
-    ),
-    windowTitle = "Shiny-tRophicPosition",
-    
-    ## ------------------------------------------------------------
-    # Carga de archivo
-    tabPanel("Dataset",
-             sidebarLayout(
-               sidebarPanel(
-                 id = 'formulario',
-                 tags$h2("Upload File", icon("fas fa-folder")),
-                 tags$hr(),
-                 
-                 radioButtons(
-                   "sep",
-                   tags$h3("Separator"),
-                   choices = c(
-                     Comma = ",",
-                     Semicolon = ";",
-                     Tab = "\t"
-                   ),
-                   selected = ",",
-                   inline = TRUE
-                 ),
-                 
-                 
-                 radioButtons(
-                   "quote",
-                   tags$h3("Comma"),
-                   choices = c(
-                     None = "",
-                     "Double Quote" = '"',
-                     "Single Quote" = "'"
-                   ),
-                   selected = '"',
-                   inline = TRUE
-                 ),
-                 
-                 switchInput(
-                   inputId = "header",
-                   label = tags$b("Have a Header?"),
-                   labelWidth = "100px",
-                   onLabel = "YES",
-                   offLabel = "NO",
-                   TRUE
-                 ),
-                 
-                 # checkboxInput("header",
-                 #               tags$b("Have a Header?"), 
-                 #               TRUE),
-                 # 
-                 
-                 fileInput(
-                   'file',
-                   tags$h3('Select File'),
-                   accept = c('text/csv',
-                              'text/comma-separated-values',
-                              '.csv'),
-                   buttonLabel = tags$b("UPLOAD"),
-                   placeholder = "Example.csv",
-                   multiple = FALSE,
-                 ),
-               ),
-               mainPanel(uiOutput("content_data"))
-             ), ),
-    ## ------------------------------------------------------------
-    tabPanel(
-      "IsotopeData Object",
-      sidebarLayout(
-        sidebarPanel(
-          id = 'formulario',
-          tags$h2('Load Isotope Data', icon("fas fa-filter")),
-          tags$hr(),
-          textInput('consumer',
-                    h4('consumer'),
-                    value = 'Bilagay'),
-          
-          textInput('consumersColumn',
-                    h4('consumersColumn'),
-                    value = 'FG'),
-          
-          textInput('b1',
-                    h4('b1'),
-                    value = 'Pelagic_BL'),
-          
-          textInput('b2',
-                    h4('b2'),
-                    value = 'Benthic_BL'),
-          
-          textInput('baselineColumn',
-                    h4('baselineColumn'),
-                    value = 'FG'),
-          
-          textInput('group',
-                    h4('group'),
-                    value = 'Coquimbo'),
-          
-          textInput('groupsColumn',
-                    h4('groupsColumn'),
-                    value = 'Location'),
-          
-          # submitButton("Enviar"),
-          
-        ),
-        mainPanel(uiOutput("content_TP"))
-      )
+shinyUI(
+  fluidPage(
+    useWaiter(),
+    useHostess(),
+    waiterShowOnLoad(
+      color = "#F1FCF8",
+      hostess_loader(
+        "loader",
+        preset = "circle",
+        text_color = "black",
+        class = "label-center",
+        center_page = TRUE,
+        fill_color = "#5fd3a7",
+        stroke_color = "#69FE46"
+      ),
       
     ),
     
-    ## ------------------------------------------------------------
-    tabPanel('Bayesian Model',
-             sidebarLayout(
-               sidebarPanel(
-                 id = 'formulario',
-                 tags$h2('Create Model', icon("fas fa-cubes")),
-                 numericInput('n.chains',
-                              h4('n.chains'),
-                              value = 4),
-                 numericInput('n.adapt',
-                              h4('n.adapt'),
-                              value = 5000),
-                 
-                 
-                 # submitButton("Enviar"),
-                 
-               ),
-               mainPanel(uiOutput("content_model"))
-             )),
     
-    ## ------------------------------------------------------------
-    tabPanel('Posterior Sample',
-             sidebarLayout(
-               sidebarPanel(
-                 id = 'formulario',
-                 tags$h2('Make Post Samples', icon("fas fa-chart-bar")),
-                 tags$hr(),
-                 numericInput('n.iter',
-                              h4('n.iter'),
-                              value = 15000),
+    tags$head(
+      tags$link(rel = "stylesheet",
+                type = "text/css",
+                href = "style.css"),
+    ),
+    tags$head(
+      HTML('<link rel="icon", href="img/logo.png",
+                                   type="image/png" />')
+    ),
+    
+    navbarPage(
+      title = div(
+        img(
+          src = 'img/logo.png',
+          style = "margin-top: -14px;
+                               padding-right:9px;
+                               padding-bottom:9px",
+          height = 100
+        )
+      ),
+      windowTitle = "Shiny-tRophicPosition",
+      
+      ## ------------------------------------------------------------
+      # Upload the file
+      tabPanel("Dataset",
+               sidebarLayout(
+                 sidebarPanel(
+                   id = 'formulario',
+                   tags$h2("Upload File", icon("fas fa-folder")),
+                   tags$hr(),
+                   
+                   radioButtons(
+                     "sep",
+                     tags$h3("Separator"),
+                     choices = c(
+                       'Comma' = ",",
+                       'Semicolon' = ";",
+                       'Tab' = "\t"
+                     ),
+                     selected = ",",
+                     inline = TRUE
+                   ),
+                   
+                   radioButtons(
+                     "quote",
+                     tags$h3("Comma"),
+                     choices = c(
+                       'None' = "",
+                       'Double Quote' = '"',
+                       "Single Quote" = "'"
+                     ),
+                     selected = '"',
+                     inline = TRUE
+                   ),
+                   
+                   radioButtons(
+                     inputId = 'header',
+                     label = tags$h3('Header'),
+                     choices = c(
+                       'Yes' = TRUE,
+                       'No' =  FALSE),
+                     selected = TRUE,
+                     inline = TRUE,
+                   ),
+
+                   selectInput(
+                     'select_a_file',
+                     tags$h3('Name of File'),
+                     choices =  c(list.files('data/'), 'New')
+                   ),
+                   
+                   uiOutput("select_file"),
+                   
+                   actionBttn(
+                     inputId = "up_file",
+                     tags$h4("UPLOAD"),
+                     style = "fill",
+                     color = "success",
+                   ),
+                   
+                   
+                 ),
+                 mainPanel(uiOutput("content_data"),
+                           #verbatimTextOutput(outputId = "res_bttn2")
+                           )
+               ),),
+      
+      ## ------------------------------------------------------------
+      #Create IsotopeData object 
+      
+      tabPanel(
+        "IsotopeData Object",
+        sidebarLayout(
+          sidebarPanel(
+            id = 'formulario',
+            tags$h2('Trophic Discrimination Factor', icon("fas fa-cog")),
+            tags$hr(),
+            selectInput(
+              inputId = "TDF_author",
+              label = tags$h4("author"),
+              choices = c('Post (2002)',
+                          'McCutchan (2003)',
+                          'New'),
+              selected = 'Post (2002)'
+            ),
+            
+            uiOutput("type_select"),
+            
+            selectInput('Idq123',
+                        h4('element'),
+                        choices = c('Both', 'N', 'C')),
+            
+            
+            tags$h2('Load Isotope Data', icon("fas fa-filter")),
+            tags$hr(),
+            textInput('consumer',
+                      h4('consumer'),
+                      value = 'Bilagay'),
+            
+            textInput('consumersColumn',
+                      h4('consumersColumn'),
+                      value = 'FG'),
+            
+            textInput('b1',
+                      h4('b1'),
+                      value = 'Pelagic_BL'),
+            
+            textInput('b2',
+                      h4('b2'),
+                      value = 'Benthic_BL'),
+            
+            textInput('baselineColumn',
+                      h4('baselineColumn'),
+                      value = 'FG'),
+            
+            textInput('group',
+                      h4('group'),
+                      value = 'Coquimbo'),
+            
+            textInput('groupsColumn',
+                      h4('groupsColumn'),
+                      value = 'Location'),
+            
+          ),
+          mainPanel(uiOutput("content_TP"))
+        )
+      ),
+      
+      ## ------------------------------------------------------------
+      #Make Posterior Samples
+      tabPanel('Posterior Sample',
+               sidebarLayout(
+                 sidebarPanel(
+                   id = 'formulario',
+                   tags$h2('Create Model', icon("fas fa-cubes")),
+                   tags$hr(),
+                   numericInput('n.chains',
+                                h4('n.chains'),
+                                value = 4),
+                   numericInput('n.adapt',
+                                h4('n.adapt'),
+                                value = 5000),
+                   
+                   actionBttn(
+                     inputId = "create_model",
+                     tags$h4("CREATE MODEL"),
+                     style = "fill",
+                     color = "success"
+                   ),
+                   
+                   uiOutput("panel_post_sample"),
+                   
+                 ),
                  
-                 # submitButton("Enviar"),
-                 
-               ),
-               mainPanel(uiOutput("content_posterior"))
-             ))
+                 mainPanel(uiOutput("content_model"))
+               )),
+    )
   )
-))
+)
